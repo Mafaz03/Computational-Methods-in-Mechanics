cc799873ba54a43edc57ad4ff958535a5fa179f4
Mafaz03 <mohdmafaz200303@gmail.com>
2025-08-16 23:49:11 +0530

    lagrange changes

--- Langrange.m
+++ Langrange.m
@@ -33,30 +33,47 @@ function [sum] = LI(xs, ys, number)
     end
 end
 
-%% Using the function
-x = [1,  2,    3,    3.2,    3.9];
-y = [1,  5,    2,    7,      4];       
 
+function [L] = Lagrange_Basis(xs, j, number)
+    L = ones(size(number));
+    n = length(xs)
+    for i = 1:n
+        if i ~= j
+            L = L .* (number - xs(i)) / (xs(j) - xs(i))
+        end
+    end
+end
 
+x = [4.0, 5.0, 6.0, 7.0, 8.0]
+y = [1.58740105, 1.709976, 1.81712059, 1.912931, 2.0]
 sample_points = 50;
 
 % Predicting
-test_xs = linspace(1, x(end), sample_points);
-test_ys = [];
-for i = 1: sample_points
-    test_ys = [test_ys, LI(x, y, test_xs(i)) ];
+test_xs = linspace(min(x), max(x), sample_points);
+
+test_ys = zeros(1, sample_points);
+for i = 1:sample_points
+    test_ys(i) = LI(x, y, test_xs(i));
+end
+
+for j = 1: length(x)
+    lb = Lagrange_Basis(x, j, test_xs);
+    plot(test_xs, lb, 'LineWidth', 1.5, 'DisplayName', sprintf('P_{%d}(x)', j));
+    hold on
 end
 
 % Plotting predicted Data
-plot(test_xs, test_ys, '--b', 'LineWidth', 1.5, 'DisplayName', 'Predicted Data points');
+plot(test_xs, test_ys, '--', 'LineWidth', 1.5, 'DisplayName', 'Predicted Data points');
 xlabel('X values');
 ylabel('Y values');
 title("Lagrange's Interpolation", 'FontSize', 25);
 
+
+
 hold on
 
 % Plotting actual Data
-plot(x, y, 'g', 'LineWidth', 1.5, 'DisplayName', 'Actual Data points', Marker='x', MarkerSize=20);
+plot(x, y, 'LineWidth', 1, 'DisplayName', 'Actual Data points', Marker='x', MarkerSize=12);
 
 legend show
 grid on;
