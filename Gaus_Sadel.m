eeda267474c3045c7d3c5562da6726543c51ef1c
Mafaz03 <mohdmafaz200303@gmail.com>
2025-08-31 12:37:43 +0530

    jacobi and gaus

--- Gaus_Sadel.m
+++ Gaus_Sadel.m
@@ -1,32 +1,39 @@
-%% Student Details
+%% Assignemnt
+% Gaus Sadel Itterator
+
+% Name        : Mohamed Mafaz
+% Roll Number : AM25M009
+% Department  : Applied Mechanics
+
 clc;
 clear;
 
 loops_taken = 0;
 
-A = [4 -1 0; 
-    -1 4 -1; 
-    0  -1 3];
+A = [4   1  -1;     % A Matrix
+     1  -5  -1; 
+     2  -1  -6];
 
-B = [15; 10; 10];
+B = [13; -8; -2];   % B Matrix
 
-X = [0; 0; 0];
+X = [0; 0; 0];      % Initial Guess
 X_new = [0; 0; 0];
 
-tolerence = 1e-9;
+tolerence = 1e-12;
 relative_error = 0;
 
+%% Part 1 (Preprocessing)
 % Checking if Diagonally Dominant
 function [] = Diag_dom(A)
     diag_dom = 0;
     for j = 1:length(A)
+        sum = 0;
         for i = 1:length(A)
-            sum = 0;
             if i ~= j
-                sum = sum + (A(j, i));
+                sum = sum + abs(A(j, i)); % abs value of sum of non diagnoal elements
             end
         end
-        if sum > A(j, j)
+        if sum > abs(A(j, j))             % Comparing to diagnol elements
             diag_dom = diag_dom + 1
         end
     end
@@ -40,27 +47,33 @@ end
 
 Diag_dom(A)
 
+%% Part 2 (Processing)
 while (relative_error > tolerence) || loops_taken == 0
     for j = 1:length(A)
         sum = 0;
         for i = 1:length(A)
             if i ~= j
-                sum = sum + (A(j, i) * X_new(i));
+                sum = sum + (A(j, i) * X_new(i));            % Sum of non diagnol elements
             end
         end
-        X_new(j) = (B(j) - sum) / A(j, j);
+        X_new(j) = (B(j) - sum) / A(j, j);                   % b - sum
+                                                             % Gauss sadel uses new values right away in the same loop
     end
 
-    relative_error = max(abs(X_new - X) ./ (X_new + 1e-9));
+    relative_error = max(abs(X_new - X) ./ (X_new + 1e-9));  % Calculating relative error
 
-    X = X_new;
+    X = X_new;                                               % Copying new array to actual array of initial guesses
     loops_taken = loops_taken + 1;
+    
+    %% Part 3 (post processing)
+    % Printing it out
+    fprintf("Loop: %d   |", loops_taken)
+    for i = 1:length(X)
+        fprintf("   X_%d: %f   |   ", i, X(i))
+    end
+    fprintf("\n")
 end
 
-
-for i = 1:length(X)
-    fprintf("X_%d: %f\n", i, X(i))
-end
 fprintf("\nLoops Taken: %d\n", loops_taken)
 fprintf("\nRelative Error: %d\n", relative_error)
 
--- Jacobi.m
+++ Jacobi.m
@@ -1,32 +1,39 @@
-%% Student Details
+%% Assignemnt
+% Jacobi Itterator
+
+% Name        : Mohamed Mafaz
+% Roll Number : AM25M009
+% Department  : Applied Mechanics
+
 clc;
 clear;
 
 loops_taken = 0;
 
-A = [4 -1 0; 
-    -1 4 -1; 
-    0  -1 3];
+A = [4   1  -1; 
+     1  -5  -1; 
+     2  -1  -6];
 
-B = [15; 10; 10];
+B = [13; -8; -2];
 
 X = [0; 0; 0];
 X_new = [0; 0; 0];
 
-tolerence = 1e-9;
+tolerence = 1e-12;
 relative_error = 0;
 
+%% Part 1 (Preprocessing)
 % Checking if Diagonally Dominant
 function [] = Diag_dom(A)
     diag_dom = 0;
     for j = 1:length(A)
+        sum = 0;
         for i = 1:length(A)
-            sum = 0;
             if i ~= j
-                sum = sum + (A(j, i));
+                sum = sum + abs(A(j, i)); % abs value of sum of non diagnoal elements
             end
         end
-        if sum > A(j, j)
+        if sum > abs(A(j, j)) % Comparing to diagnol elements
             diag_dom = diag_dom + 1
         end
     end
@@ -40,6 +47,7 @@ end
 
 Diag_dom(A)
 
+%% Part 2 (Processing / Using the function)
 while (relative_error > tolerence) || loops_taken == 0
     for j = 1:length(A)
         sum = 0;
@@ -55,12 +63,15 @@ while (relative_error > tolerence) || loops_taken == 0
 
     X = X_new;
     loops_taken = loops_taken + 1;
+
+    % Printing it out
+    fprintf("Loop: %d   |", loops_taken)
+    for i = 1:length(X)
+        fprintf("   X_%d: %f   |   ", i, X(i))
+    end
+    fprintf("\n")
 end
 
-
-for i = 1:length(X)
-    fprintf("X_%d: %f\n", i, X(i))
-end
 fprintf("\nLoops Taken: %d\n", loops_taken)
 fprintf("\nRelative Error: %d\n", relative_error)
 
