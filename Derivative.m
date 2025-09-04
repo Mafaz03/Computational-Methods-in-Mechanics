afca33eb4da0e6867a092a82508301a7cbe282be
Mafaz03 <mohdmafaz200303@gmail.com>
2025-09-04 13:41:01 +0530

    derivative

---
+++ Derivative.m
@@ -0,0 +1,54 @@
+clc;
+clear;
+close all;
+
+f = @(x) cos(x);
+f_actual = @(x) -sin(x);
+x = 1;
+
+function [val] = forward_method(f, x, h)
+    val = (f(x+h) - f(x))/h;
+end
+
+function [val] = backward_method(f, x, h)
+    val = (f(x) - f(x-h))/ h ;
+end
+
+function [val] = central_method(f, x, h)
+    val = ( f(x+h) - f(x-h) ) / (2*h);
+end
+
+
+steps = 50;
+hs = logspace(-15, -1, steps);
+
+error_forward = zeros(1, steps);
+error_backward = zeros(1, steps);
+error_central = zeros(1, steps);
+
+for i = 1:steps
+    forwardVal  = forward_method(f, x, hs(i));
+    backwardVal = backward_method(f, x, hs(i));
+    centralVal = central_method(f, x, hs(i));
+
+    error_forward(i)  = abs(forwardVal - f_actual(x));
+    error_backward(i) = abs(backwardVal - f_actual(x));
+    error_central(i)  = abs(centralVal - f_actual(x));
+end
+
+disp(error_forward)
+disp(error_backward)
+disp(error_central)
+
+loglog(hs, error_forward, 'DisplayName', 'Forward error')
+hold on
+loglog(hs, error_backward, 'DisplayName', 'Backward error')
+hold on
+loglog(hs, error_central, 'DisplayName', 'Central error')
+
+
+
+legend show;
+xlabel('h');
+ylabel('abs error');
+
