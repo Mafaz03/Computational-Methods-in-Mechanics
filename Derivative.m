6b7fcd5ab5eec8dbc2080d9a1de36e3b64ac4796
Mafaz03 <mohdmafaz200303@gmail.com>
2025-09-22 22:47:48 +0530

    assignment 5

--- Derivative.m
+++ Derivative.m
@@ -19,6 +19,10 @@ function [val] = central_method(f, x, h)
     val = ( f(x+h) - f(x-h) ) / (2*h) ;
 end
 
+function [val] = central_method_h4(f, x, h)
+    val = ( -f(x+2*h) + (8 * f(x+h)) - (8 * f(x-h)) + f(x-2*h)) / (12*h) ;
+end
+
 
 steps = 50;
 hs = logspace(-2, -10, steps);
@@ -26,31 +30,37 @@ hs = logspace(-2, -10, steps);
 error_forward = zeros(1, steps);
 error_backward = zeros(1, steps);
 error_central = zeros(1, steps);
+error_central_h4 = zeros(1, steps);
 
 for i = 1:steps
     forwardVal  = forward_method(f, x, hs(i));
     backwardVal = backward_method(f, x, hs(i));
     centralVal = central_method(f, x, hs(i));
+    centralVal_h4 = central_method_h4(f, x, hs(i));
 
     error_forward(i)  = abs(forwardVal - f_actual(x));
     error_backward(i) = abs(backwardVal - f_actual(x));
     error_central(i)  = abs(centralVal - f_actual(x));
+    error_central_h4(i)  = abs(centralVal_h4 - f_actual(x));
 end
 
 disp(error_forward)
 disp(error_backward)
 disp(error_central)
+disp(error_central_h4)
 
 loglog(hs, error_forward, 'DisplayName', 'Forward error')
 hold on
 loglog(hs, error_backward, 'DisplayName', 'Backward error')
 hold on
 loglog(hs, error_central, 'DisplayName', 'Central error')
+hold on
+loglog(hs, error_central_h4, 'DisplayName', 'Central error O(h^4)')
 
 
 
 legend show;
-grid on;
+% grid on;
 xlabel('h');
 ylabel('abs error');
 
