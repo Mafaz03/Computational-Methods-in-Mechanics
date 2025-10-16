46950eb72b7e2c1d2c46b30723537113328742ed
Mafaz03 <mohdmafaz200303@gmail.com>
2025-10-16 14:08:48 +0530

    PDE

--- PDE_class.m
+++ PDE_class.m
@@ -1,8 +1,8 @@
-clc
-clear
+% clc
+% clear
 close all
 
-nodes = 50;
+nodes = 10;
 
 matrix      = zeros(nodes,nodes);
 
@@ -13,9 +13,12 @@ matrix(nodes, :) = 10;  % Top
 matrix(:, 1)     = 0;   % Left
 matrix(:, nodes) = 0;   % Right
 
+coses = cos(pi/(nodes-1)) + cos(pi/(nodes-1));
+w = 4 / (2 + sqrt(4 - coses^2)); % change this later on
+% w = 1;
 
 tol = 1e-5;
-abs_err = tol+1;
+abs_err = tol+1; % To allow it once lol
 loops = 1;
 
 while abs_err > tol
@@ -23,7 +26,13 @@ while abs_err > tol
     for i = 2:nodes-1
         for j = 2:nodes-1
            
-            matrix(i,j) = (matrix_old(i+1, j) + matrix_old(i-1, j) + matrix_old(i, j+1) + matrix_old(i, j-1)) / 4;
+            % No relaxation
+            % matrix(i,j) = (matrix(i+1, j) + matrix(i-1, j) + matrix(i, j+1) + matrix(i, j-1)) / 4;
+
+            % relaxation
+            s =  (matrix_old(i+1, j) + matrix(i-1, j) + matrix(i, j+1) + matrix(i, j-1) - 4 * matrix_old(i, j)) / 4;
+            matrix(i,j) = matrix_old(i, j) + (w * s);
+
         end
     end
 
@@ -40,7 +49,7 @@ figure;
 contour(X, Y, matrix);
 title('Contour lines');
 
-% Filled contour
+% contour
 figure;
 contourf(X, Y, matrix);
 title('Filled contour');
